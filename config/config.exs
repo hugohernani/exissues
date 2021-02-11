af91a52b4f933ace262f1f7ba1692522ad510fb3
Hugo Hernani <hhernanni@gmail.com>
2021-02-11 12:05:30 -0300

    Add some logging

--- config/config.exs
+++ config/config.exs
@@ -1,2 +1,3 @@
 use Mix.Config
 config :issues, github_url: "https://api.github.com"
+config :logger, compile_time_purge_level: :info
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -13,6 +17,9 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status cide=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       status_code |> check_for_error(),
       body |> Poison.Parser.parse!()
