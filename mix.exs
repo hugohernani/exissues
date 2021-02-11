0b97d6351336f438de10527d7fe02cc6f4bf580f
Hugo Hernani <hhernanni@gmail.com>
2021-02-11 11:54:42 -0300

    Package project with escript

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
@@ -22,8 +23,14 @@ defmodule Issues.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.0.0"},
-      { :poison,   "~> 3.1" },
+      {:poison, "~> 3.1"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
